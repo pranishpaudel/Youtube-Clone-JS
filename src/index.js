321e83e5f86c0f40abdcf8e7d1cf01370314ce24
Pranish <pranishpoudel01@gmail.com>
2024-01-06 20:15:33 +0545

    fix: :bug: Fix MongoDB connection Bug

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+import {DB_NAME} from './constants.js';
+import connectDB from './db/index.js';
+import 'dotenv/config';
+
+connectDB();
+
+// const app= express();
+// (async ()=>{
+// try{
+// await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+// app.on("error",(error)=>{
+//     console.log("Error",error);
+//     throw error;
+// })
+// app.listen(process.env.PORT, ()=>{
+//     console.log(`App is listening on port ${process.env.PORT}`);
+// })
+
+// }
+// catch(error){
+//     console.log("Error",error)
+
+// }
+// })()
