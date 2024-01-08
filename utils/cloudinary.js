0f8eb2833a2f27179218c5b56c89927931a86cb6
Pranish <pranishpoudel01@gmail.com>
2024-01-08 14:58:33 +0545

    feat: :zap: Introduced Cloudinary Service

---
+++ utils/cloudinary.js
@@ -0,0 +1,27 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs';
+
+
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_SECRET_KEY 
+});
+
+
+const uploadOnCloudinary= async(localFilePath)=> {
+    try{
+        if (!localFilePath) return null
+        //upload the file on cloudinary
+       const response= await cloudinary.v2.uploader.upload(localFilePath,{
+            resource_type: 'auto'
+        });
+        console.log("file is successfully uploaded",response.url)
+        return response
+        //file has been uploaded successfully
+    }
+catch(error){
+fs.unlinkSync(localFilePath);
+return null;
+}}
