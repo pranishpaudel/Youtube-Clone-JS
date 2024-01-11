a4794efd6799bd83cc8d7d3ee924029e9accdcfc
Pranish <pranishpoudel01@gmail.com>
2024-01-11 18:13:36 +0545

    feat: :zap: Introduced Registration Verification Middlewares And Error Handling

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,23 @@
+import { ApiError } from '../../utils/ApiError.js';
 import {asyncHandler} from '/Users/air/Desktop/Youtube Clone JS/utils/asyncHandler.js';
-
+import { User } from '../models/user.models.js';
 
 
 
 export const registerUser= asyncHandler(async (req,res) =>{
-res.status(200).json({
-    message: 'ok'
-})
+    const {fullname, email, username, password}= req.body;
+    if(
+        [fullname, email, username, password].some((field)=>field===undefined || field.trim==='')
+       
+    ){
+        throw new ApiError(400,"All fields are required");
+    }
+    const existedUser= User.findOne({
+        $or : [{username}, {email}]
+    })
+    if(existedUser){
+        throw new ApiError(409,"User with email or username already exists");
+    }
+    console.log(fullname, email, username, password);
+
 })
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,19 @@
 import { Router } from "express";
 import { registerUser } from "/Users/air/Desktop/Youtube Clone JS/src/controllers/user.controller.js";
-
-
+import { upload } from '../middlewares/multer.middleware.js';
+import { ApiError } from "/Users/air/Desktop/Youtube Clone JS/utils/ApiError.js";
 
 const router= Router();
 
+router.route("/register").post(upload.fields([
+    {
+        name: 'avatar',
+        maxCount: 1,
+    },
+    {
+        name: 'coverImage',
+        maxCount: 1,
+    }
+]), registerUser);
 
-router.route("/register").post(registerUser);
 export default router;
